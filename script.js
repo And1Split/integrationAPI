9e3b528304fda0414b0f33145cfe5685d0982f2c
Nils G <nils.gillet@gmail.com>
2016-03-01 08:16:48 +0000

    Finished code

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function readyFn() {
 
 
     //----- FUNCTIONS ------
-
+    // Initiate the carsouel element
     function intitCarousel() {
         "use strict";
         $('#carousel_container').append(
@@ -40,11 +40,11 @@ function readyFn() {
             $('<ul>', {
                 id: 'carousel'
             }));
-        return 1;
+        return 1; //Used to check if the creation was successful.
     }
 
-    function addImage(imageObj) {
-        "use strict";
+    function addImage(imageObjThumb, imageObjOriginal) {
+        "use strict"; //Needed in order to declare new DOM element using the below method
         $(imageContainer).append(
             $('<li>', {
                 class: 'image_bloc'
@@ -53,10 +53,60 @@ function readyFn() {
                     class: 'align_helper'
                 })).append(
                 $('<img>', {
-                    src: imageObj
+                    src: imageObjThumb
+                }).click(function() {
+                    popInImage(imageObjOriginal); //Adding the click element to open the popin.
                 })
             )
         )
+    }
+
+    //Inserting the image 'original' in the popin
+    //Receive the 'original' image URL as a parameter
+    function originalImageEl(imgURL) {
+        var el;
+        el = $('<div>', {
+            id: 'popin_container'
+        }).append($('<img>', {
+            src: imgURL,
+            id: 'popin_image'
+        }))
+        return el;
+    }
+
+    //Creating the modal box
+    function popInImage(img) {
+        var block_page = $('<div id="block_page"></div>'); // Create the blocker
+        block_page.appendTo('body');
+        block_page.append(originalImageEl(img));
+        //Adding the close button + related animations
+        $('#popin_container').append($('<div id="close_window">X</div>').click(function() {
+            $('#popin_container').animate({
+                opacity: 0
+            }, 200, function() {
+                $('#block_page').animate({
+                    opacity: 0
+                }, 200, function() {
+                    $('#popin_container').remove();
+                    $('#block_page').remove();
+                });
+            });
+        }));
+        // Checking the size of the image to apply the right classs to insure the image is automatically added in the middle of the window
+        if ($('#popin_image').naturalWidth > $('#popin_image').naturalHeight) {
+            $('#popin_image').addClass('horizontal');
+        } else {
+            $('#popin_image').addClass('vertical');
+        }
+        //Animate the image display once it has fully loaded
+        $('#popin_image').load(function() {
+            $('#popin_container').animate({
+                opacity: 1
+            }, 500, function() {
+                // Animation complete.
+            });
+        });
+        console.log(img);
     }
 
     // Retrieve the media base URL in order to retrieve content.
@@ -72,7 +122,7 @@ function readyFn() {
                 dataType: 'json', // define data type that we're going to be receiving
                 success: function(media) {
                     if (media.data._embedded.length > 0) {
-                        if (intitCarousel()) {
+                        if (intitCarousel()) { // If the carousel has initiated, adding the images and the carousel logic 
                             $("#right_arrow_container").click(function() {
                                 $("#carousel").animate({
                                     left: "-=158"
@@ -91,9 +141,9 @@ function readyFn() {
                                     $("#carousel").clearQueue();
                                 });
                             });
-                            console.log('In');
+                            //Running through all the images in the JSON objesct and adding them to the carousel
                             $.each(media.data._embedded, function(index, obj) {
-                                addImage(obj.images.thumbnail);
+                                addImage(obj.images.thumbnail, obj.images.original);
                             })
                         } else {
                             console.log("Error: Carousel not initiated.")
@@ -101,7 +151,6 @@ function readyFn() {
                     } else {
                         console.log("Error: No image available.")
                     }
-                    console.log(media);
                 },
                 error: function(error) {
                     // Log error
